d9e5232cc06963038c49e5463c4bc7d816d7902c
Jeremy Caine <jezcaine@gmail.com>
2020-06-02 20:48:08 +1000

    3001

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var port = 3001;
 
 console.log("host: %s", host);
 console.log("port: %s", port);
+console.log("hello");
 
 var version = process.env.VERSION;
 var cors_allow = process.env.CORS_ALLOW || '*';
